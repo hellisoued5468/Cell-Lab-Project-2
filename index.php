22ba4f2b3e89bc2a0a3b8e23869655254ea2e802
hellisoued\이준현 <hellisoued1468@gmail.com>
2017-08-29 18:02:42 +0900

    Cell Lab Wiki 2.02 added TB

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="ko">
   <head>
-    
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="style.css">
+    <link href="http://localhost/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
     <title>CellLab Wiki</title>
 
     <?php
     //php Head Zone
       require("widget/DB.php")
-
      ?>
   </head>
     <body>
@@ -29,6 +30,7 @@
         require "widget/article.php";
        ?>
     </article>
-
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <script src="http://localhost/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
   </body>
 </html>
