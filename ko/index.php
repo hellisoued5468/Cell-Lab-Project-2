00be82a7e3f9f0846ad382e383703345c38ac312
hellisoued\이준현 <hellisoued1468@gmail.com>
2017-11-18 18:25:50 +0900

    2.10 언어를 구분하였음.

--- ko/index.php
+++ ko/index.php
@@ -11,8 +11,8 @@
       <meta name="naver-site-verification" content="5d53a9a1d2540aef8081ee90b473b941a70631e7"/>
     </div>
     <div class="link-stylesheet">
-      <link rel="stylesheet" href="style.css">
-      <link rel="stylesheet" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
+      <link rel="stylesheet" href="http://localhost/style.css">
+      <link rel="stylesheet" href="http://localhost/bootstrap-3.3.7-dist/css/bootstrap.min.css">
     </div>
     <title>CellLab Wiki</title>
     <?php
@@ -37,7 +37,7 @@
       require "widget/article.php";
      ?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <script src="http://localhost/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
   </body>
 </html>
 <a class="btn btn-default" href="/filepush.html" role="button">개발자에게 텍스트보내기(영어만요!제발!)</a>
